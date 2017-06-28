e0a0764f27f3889f6d04da2c3a8080b543ae60d4
Connor McCurrie <c.mccurrie@gmail.com>
2017-06-28 08:12:58 -0500

    phone formatting

--- js/script.js
+++ js/script.js
@@ -32,6 +32,21 @@ var map;
 var client_ID;
 var client_Secret;
 
+function formatPhone(phonenum) {
+    var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (regexObj.test(phonenum)) {
+        var parts = phonenum.match(regexObj);
+        var phone = "";
+        if (parts[1]) { phone += "+1 (" + parts[1] + ") "; }
+        phone += parts[2] + "-" + parts[3];
+        return phone;
+    }
+    else {
+        //invalid phone number
+        return phonenum;
+    }
+}
+
 var Location = function (data) {
 	var self = this;
 	this.name = data.name;
