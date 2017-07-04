eaaf465a6051c695282e7ee15098253db79ac374
Connor McCurrie <c.mccurrie@gmail.com>
2017-07-03 19:24:03 -0500

    Styles change
    Going with a more mobile minded framework. Various style changes to match the whitespace. Now to work on the knockout.

--- js/app.js
+++ js/app.js
@@ -1,50 +1,169 @@
-//Model
-var locations = [
-  {
-    name: "Devil's Lake State Park",
-    lat: 43.428447,
-    lng: -89.731368
-  },
-  {
-    name: "High Cliff State Park",
-    lat: 44.163103,
-    lng: -88.29097
-  },
-  {
-    name: "Harrington Beach State Park",
-    lat: 43.492876,
-    lng: -87.803664
-  },
-  {
-    name: "Kohler-Andrae State Park",
-    lat: 43.664985,
-    lng: -87.719721
-  },
-  {
-    name: "Blue Mound State Park",
-    lat: 43.0295,
-    lng: -89.840716
-  }
-];
-
-$(document).ready(function() {
-  ko.applyBindings(viewModel);
-});
-
-function ViewModel() {
-  var self = this;
-  self.mapOne = ko.observable({
-    lat: ko.observable(43.428447),
-    lng: ko.observable(-89.731368)
+//Map constructors 
+var map;
+function initMap() {
+  var styles = [
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": "50"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "30"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "40"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffff00"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    }
+]
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat:43.78444, lng: -88.787868},
+    zoom: 8,
+    mapTypeControl: false,
+    styles: styles,
+    MapTypeId: 'hybrid'
   });
-
 }
 
-ko.bindingHandler.maps = {
-  init: function(element, valueAccessor, allBindingsAccessor, viewModel){
-    var mapObj = ko.utils.unwrapObservable(valueAccessor());
-    var latLng = new google.maps.LatLng(
-      ko.)
-  }
+// //Marker constructor
+// function Location() {
+//   var self = this;
+//   self.locations = ko.observableArray([
+//       {
+//       name: "Devil's Lake State Park",
+//       lat: 43.428447,
+//       lng: -89.731368
+//     },
+//     {
+//       name: "High Cliff State Park",
+//       lat: 44.163103,
+//       lng: -88.29097
+//     },
+//     {
+//       name: "Harrington Beach State Park",
+//       lat: 43.492876,
+//       lng: -87.803664
+//     },
+//     {
+//       name: "Kohler-Andrae State Park",
+//       lat: 43.664985,
+//       lng: -87.719721
+//     },
+//     {
+//       name: "Blue Mound State Park",
+//       lat: 43.0295,
+//       lng: -89.840716
+//     }
+//   ]);
+// }
 
-}
