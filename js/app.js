d9b0bfed109a97e88bdc0c8e5633570d45f0cdb8
Connor McCurrie <c.mccurrie@gmail.com>
2017-07-06 13:36:16 -0500

    Adding marker functionality
    Adding marker functionality, map views, playing with data bindings

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
-//Map constructors 
+var locations = [
+    { 
+      name: "Devil's Lake State Park",
+      lat: 43.428447,
+      lng: -89.731368
+    },
+    { 
+      name: "High Cliff State Park",
+      lat: 44.163103,
+      lng: -88.29097
+    },
+    { 
+      name: "Harrington Beach State Park",
+      lat: 43.492876,
+      lng: -87.803664
+    },
+    { 
+      name: "Kohler-Andrae State Park",
+      lat: 43.664985,
+      lng: -87.719721
+    },
+    { 
+      name: "Blue Mound State Park",
+      lat: 43.0295,
+      lng: -89.840716
+    },
+    { 
+      name: "Rocky Arbor State Park",
+      lat: 43.641944,
+      lng: -89.805833
+    },
+    { 
+      name: "Governor Dodge State Park",
+      lat: 43.027903,
+      lng: -90.110189
+    },
+    
+];
+
+// Map constructors 
 var map;
+var marker;
+
 function initMap() {
   var styles = [
     {
@@ -126,44 +167,30 @@ function initMap() {
         ]
     }
 ]
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:43.78444, lng: -88.787868},
+    center: {lat:43.78444, lng:-88.787868},
     zoom: 8,
     mapTypeControl: false,
     styles: styles,
-    MapTypeId: 'hybrid'
+    MapTypeId: google.maps.MapTypeId.HYBRID
   });
+
+};
+
+function CampViewModel(){
+  var self = this;
+  this.name = ko.observable();
+  this.lat = ko.observable();
+  this.lng = ko.observable();
+
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].location;
+    var name = locations[i].name;
+    var marker = new.google.maps.Marker({
+      position: location,
+      map: map,
+      animation: google.maps.animation.DROP
+    });
+  }
 }
-
-// //Marker constructor
-// function Location() {
-//   var self = this;
-//   self.locations = ko.observableArray([
-//       {
-//       name: "Devil's Lake State Park",
-//       lat: 43.428447,
-//       lng: -89.731368
-//     },
-//     {
-//       name: "High Cliff State Park",
-//       lat: 44.163103,
-//       lng: -88.29097
-//     },
-//     {
-//       name: "Harrington Beach State Park",
-//       lat: 43.492876,
-//       lng: -87.803664
-//     },
-//     {
-//       name: "Kohler-Andrae State Park",
-//       lat: 43.664985,
-//       lng: -87.719721
-//     },
-//     {
-//       name: "Blue Mound State Park",
-//       lat: 43.0295,
-//       lng: -89.840716
-//     }
-//   ]);
-// }
-
