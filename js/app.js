6403baecc487862b3f4c6c581a5d8fe0be8d417f
Connor McCurrie <c.mccurrie@gmail.com>
2017-07-09 13:48:53 -0500

    Markers
    Working on making markers appear properly.

--- js/app.js
+++ js/app.js
@@ -1,79 +1,79 @@
-// Location Data
-var locations = [
-    { 
-      name: "Devil's Lake State Park",
-      lat: 43.428447,
-      lng: -89.731368
-    },
-    { 
-      name: "High Cliff State Park",
-      lat: 44.163103,
-      lng: -88.29097
-    },
-    { 
-      name: "Harrington Beach State Park",
-      lat: 43.492876,
-      lng: -87.803664
-    },
-    { 
-      name: "Kohler-Andrae State Park",
-      lat: 43.664985,
-      lng: -87.719721
-    },
-    { 
-      name: "Blue Mound State Park",
-      lat: 43.0295,
-      lng: -89.840716
-    },
-    { 
-      name: "Rocky Arbor State Park",
-      lat: 43.641944,
-      lng: -89.805833
-    },
-    { 
-      name: "Governor Dodge State Park",
-      lat: 43.027903,
-      lng: -90.110189
-    }
-    
-];
-
 // Map constructor
 var map;
 var markers = [];
 
-var Place = function (data) {
-  this.name = ko.observable(data.name);
-  this.lat = ko.observable(data.lat);
-  this.lng = ko.observable(data.lng);
-
+function initMap(data) {
+  var LatLng = {lat:43.78444, lng:-88.787868};
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:43.78444, lng:-88.787868},
+    center: LatLng,
     zoom: 8,
     mapTypeControl: false,
     MapTypeId: google.maps.MapTypeId.HYBRID
   });
 
-  infoWindow = new google.maps.InfoWindow();
-  this.marker = new google.maps.Marker({
-    position: new google.maps.LatLng(),
-    map: map,
-    title: this.name
-  });
+    // Location Data
+  var locations = [
+      { 
+        name: "Devil's Lake State Park",
+        lat: 43.428447,
+        lng: -89.731368
+      },
+      { 
+        name: "High Cliff State Park",
+        lat: 44.163103,
+        lng: -88.29097
+      },
+      { 
+        name: "Harrington Beach State Park",
+        lat: 43.492876,
+        lng: -87.803664
+      },
+      { 
+        name: "Kohler-Andrae State Park",
+        lat: 43.664985,
+        lng: -87.719721
+      },
+      { 
+        name: "Blue Mound State Park",
+        lat: 43.0295,
+        lng: -89.840716
+      },
+      { 
+        name: "Rocky Arbor State Park",
+        lat: 43.641944,
+        lng: -89.805833
+      },
+      { 
+        name: "Governor Dodge State Park",
+        lat: 43.027903,
+        lng: -90.110189
+      }
+    ];
+
+    for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].location;
+    var title = locations[i].name;
+    var  marker = new google.maps.Marker({
+        position: position,
+        title: name,
+        animation: google.maps.Animation.DROP,
+        name: name,
+        id: i
+      });
+    markers.push(marker);
+  }
+
+
+  var Place = function (map, data) {
+  this.name = ko.observable(data.name);
+  this.lat = ko.observable(data.lat);
+  this.lng = ko.observable(data.lng);
 };
+}
 
-function ViewModel() {
-  var self = this;
+// function ViewModel() {
+//   var self = this;
 
-  this.locationList = ko.observableArray([]);
+// };
 
-  locations.forEach(function(locationItem){
-    self.locationList.push(new Place(locationItem) );
-  });
-
-  this.currentPlace = ko.observable(this.locationList[0]);
-
-};
-
-ko.applyBindings(new ViewModel());
 
