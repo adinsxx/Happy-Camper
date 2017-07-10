54d334e26b4a5e5da879a69ca0067d78da1c3be5
Connor McCurrie <c.mccurrie@gmail.com>
2017-07-10 14:13:13 -0500

    Change up
    Separating the model and the viewmodel from each other because I keep getting a headache.

---
+++ js/model.js
@@ -0,0 +1,37 @@
+  var locations = [
+      { 
+        title: "Devil's Lake State Park",
+        lat: 43.428447,
+        lng: -89.731368
+      },
+      { 
+        title: "High Cliff State Park",
+        lat: 44.163103,
+        lng: -88.29097
+      },
+      { 
+        title: "Harrington Beach State Park",
+        lat: 43.492876,
+        lng: -87.803664
+      },
+      { 
+        title: "Kohler-Andrae State Park",
+        lat: 43.664985,
+        lng: -87.719721
+      },
+      { 
+        title: "Blue Mound State Park",
+        lat: 43.0295,
+        lng: -89.840716
+      },
+      { 
+        title: "Rocky Arbor State Park",
+        lat: 43.641944,
+        lng: -89.805833
+      },
+      { 
+        title: "Governor Dodge State Park",
+        lat: 43.027903,
+        lng: -90.110189
+      }
+    ];
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -11,45 +11,6 @@ function initMap(data) {
     MapTypeId: google.maps.MapTypeId.HYBRID
   });
 
-    // Location Data
-  var locations = [
-      { 
-        title: "Devil's Lake State Park",
-        lat: 43.428447,
-        lng: -89.731368
-      },
-      { 
-        title: "High Cliff State Park",
-        lat: 44.163103,
-        lng: -88.29097
-      },
-      { 
-        title: "Harrington Beach State Park",
-        lat: 43.492876,
-        lng: -87.803664
-      },
-      { 
-        title: "Kohler-Andrae State Park",
-        lat: 43.664985,
-        lng: -87.719721
-      },
-      { 
-        title: "Blue Mound State Park",
-        lat: 43.0295,
-        lng: -89.840716
-      },
-      { 
-        title: "Rocky Arbor State Park",
-        lat: 43.641944,
-        lng: -89.805833
-      },
-      { 
-        title: "Governor Dodge State Park",
-        lat: 43.027903,
-        lng: -90.110189
-      }
-    ];
-
     for (var i = 0; i < locations.length; i++) {
     var position = {lat: locations[i].lat, lng: locations[i].lng};
     var title = locations[i].title;
@@ -61,7 +22,6 @@ function initMap(data) {
         id: i
       });   
       markers.push(marker)
-
   }
 }
 
