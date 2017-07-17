743026909b1f57fc86947645973e1afcd7de8ccb
Connor McCurrie <c.mccurrie@gmail.com>
2017-07-17 09:26:20 -0500

    Binding issues?

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -7,7 +7,8 @@ var ViewModel = function (data) {
   var self = this;
   this.title = ko.observableArray([]);
   this.locationList = ko.observableArray([]);
-  this.query = ko.observable();
+  this.filter = ko.observable('');
+
 
   locations.forEach(function (locationItem) {
   	self.locationList.push(new Location(locationItem) );
@@ -18,5 +19,21 @@ var ViewModel = function (data) {
 		google.maps.event.trigger(location.marker, 'click')
   };
 
-  
+  this.filteredPlaces = ko.computed(function() {
+  	var filter = self.filter().toLowerCase();
+  	if(!filter) {
+  		locations.forEach(function(location) {
+  			if (location.marker) {
+  						location.marker.setVisible(true);
+  			}
+  		});
+  			return location
+  	} else {
+  		return ko.utils.arrayFilter(location, function(location) {
+  			var searchValue = location.name.toLowerCase().indexOf(filter) !== -1;
+  			location.marker.setVisible(searchValue);
+  			return searchValue
+  		});
+  	}
+  });	
 };
