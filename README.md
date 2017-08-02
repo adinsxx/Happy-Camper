1114e16a733fe7b6e04493e4d1b37d470c182b39
Connor McCurrie <c.mccurrie@gmail.com>
2017-08-01 20:27:59 -0500

    Review edits made

--- README.md
+++ README.md
@@ -3,4 +3,18 @@ Happy Camper
 This is my submission for the Udacity FSND Project 6: Neighborhood Map. I love to camp and I wanted to share a few of my favorite spots. 
 It was made using google maps and foursquare APIs. The framework is KnockoutJS. 
 
-You can check it out here!:  https://adinsxx.github.io/Happy-Camper/
+To run it locally, you need to follow these instructions:
+1. Clone to your github desktop or download the zip file
+2. If you've chosen to download the file, extract the files from the zip file
+3. Once finished, make your way to the 'src' folder
+4. When in the 'src' folder, navigate to the index.html file. Open it in a text editor of your choice and scroll down to line 50
+5. Once there, you will see the GoogleAPI link. I have removed my MAPKEY and this will allow you to input your own
+(I have marked where you need to input your key as YOURKEYHERE so its easy to see!)
+6. To get your own GoogleAPI key, head here!
+7. Once you have your key, have it take the place of YOURKEYHERE and then open the index.html file in the browser of your choice
+8. You did it!
+
+
+You can also check out the live version here:  https://adinsxx.github.io/Happy-Camper/
+
+Thanks for checking out my app!
---
+++ src/js/viewmodel.js
@@ -0,0 +1,40 @@
+var Location = function(data) {
+    this.name = data.title;
+    this.marker = data.marker;
+}
+
+var ViewModel = function() {
+    var self = this;
+    this.title = ko.observableArray([]);
+    this.locationList = ko.observableArray([]);
+    this.filter = ko.observable('');
+
+
+    locations.forEach(function(locationItem) {
+        self.locationList.push(new Location(locationItem));
+    });
+
+    this.activateMarker = function(location) {
+        console.log(location)
+        google.maps.event.trigger(location.marker, 'click')
+    };
+
+    this.filteredPlaces = ko.computed(function() {
+        var filter = self.filter().toLowerCase();
+        if (!filter) {
+            self.locationList().forEach(function(location) {
+                if (location.marker) {
+                    location.marker.setVisible(true);
+                }
+            });
+            return self.locationList()
+        } else {
+            return ko.utils.arrayFilter(self.locationList(), function(location) {
+                console.log(location);
+                var searchValue = location.name.toLowerCase().indexOf(filter) !== -1;
+                location.marker.setVisible(searchValue);
+                return searchValue
+            });
+        }
+    });
+};
